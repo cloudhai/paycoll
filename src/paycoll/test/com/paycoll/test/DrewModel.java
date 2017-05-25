bf74140e0aa85936e1f20cba4abe4c03b60cd18b
cloudhai <amingb-518@163.com>
2017-05-25 11:47:44 +0800

    二炮手

---
+++ src/paycoll/test/com/paycoll/test/DrewModel.java
@@ -0,0 +1,25 @@
+package com.paycoll.test;
+
+/**
+ * Created by cloud on 2017/5/25.
+ */
+public class DrewModel {
+    private int total;
+    private int value;
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
